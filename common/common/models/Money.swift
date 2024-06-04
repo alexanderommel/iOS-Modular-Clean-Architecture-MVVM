8daae212c0f9476c1eac9bbc78380d4d428ee883
Rommel <alexanderommelsw@outlook.es>
2024-06-03 22:41:20 -0500

    Create Money.swift

---
+++ common/common/models/Money.swift
@@ -0,0 +1,27 @@
+//
+//  Money.swift
+//  common
+//
+//  Created by Rommel Valdiviezo on 3/6/24.
+//
+
+import Foundation
+
+public struct Money: Codable, Hashable {
+    public enum Currency: String, Codable {
+      case USD, EUR, GBP, CNY // supported currencies here
+    }
+    public var amount: Decimal
+    public var currency: Currency
+    public var formatedAmount: String{
+        get{
+            return "\(amount) $"
+        }
+    }
+    
+    public init(amount: Decimal, currency: Currency) {
+        self.amount = amount
+        self.currency = currency
+    }
+    
+}
