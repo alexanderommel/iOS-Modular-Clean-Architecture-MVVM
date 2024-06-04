ccde930d321dc8f6dfecb2162cd4404366431f97
Rommel <alexanderommelsw@outlook.es>
2024-06-03 22:29:30 -0500

    stores screen

--- features/stores/stores-ui/stores-ui/screens/StoresScreen.swift
+++ features/stores/stores-ui/stores-ui/screens/StoresScreen.swift
@@ -7,22 +7,63 @@
 
 import SwiftUI
 import stores
+import common
 
-public struct StoresScreen: View {
+struct StoresScreen: View {
     
-    @Environment(StoresViewModel.self) var vm
+    var categories: [StoreCategory]
+    var stores: [Store]
     
+    var locc = Location(address: "asd", latitude: "", longitude: "")
     
-    public var body: some View {
-        List{
-            ForEach(vm.stores){ store in
-                Text(store.id.description)
+    var body: some View {
+        
+        NavigationStack{
+            List{
+                HStack{
+                    Text("¡Hola Alexander!")
+                        .foregroundStyle(.black)
+                        .fontWeight(.bold)
+                        .font(.headline)
+                }
+                .listRowSeparator(.hidden)
+
+                Text("Mira lo que tenemos para ti.")
+                    .foregroundStyle(Color.myText34Color)
+                    .fontWeight(.semibold)
+                    .font(.subheadline)
+                
+                LocationView()
+                    .padding(.top, 2)
+                    .listRowSeparator(.hidden)
+                
+                StoreCategoryRowView(categories: categories)
+                    .listRowSeparator(.hidden)
+                
+                Text("Restaurantes disponibles")
+                    .foregroundStyle(.black)
+                    .fontWeight(.bold)
+                    .font(.headline)
+                    .padding(.top, 4)
+                    .listRowSeparator(.hidden)
+                
+                ForEach(stores) { store in
+                    NavigationLink
+                    {
+                    } label: {
+                        StoreView(store: store)
+                    }
+                    
+                }
             }
+            .navigationTitle("Restaurantes")
+            .listStyle(.plain)
         }
+        
+        
     }
 }
 
 #Preview {
-    StoresScreen()
-        .environment(StoresViewModel(repository: StoreRemoteRepositoryMock()))
+    StoresScreen(categories: restaurant_categories, stores: stors)
 }
