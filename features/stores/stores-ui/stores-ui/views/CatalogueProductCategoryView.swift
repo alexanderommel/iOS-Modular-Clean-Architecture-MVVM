316ccc3330ff36e053948b3b3fc80ad06b96cbdc
Rommel <alexanderommelsw@outlook.es>
2024-06-03 22:42:02 -0500

    catalogue product category view

---
+++ features/stores/stores-ui/stores-ui/views/CatalogueProductCategoryView.swift
@@ -0,0 +1,33 @@
+//
+//  CatalogueProductCategoryView.swift
+//  stores-ui
+//
+//  Created by Rommel Valdiviezo on 3/6/24.
+//
+
+import SwiftUI
+import stores
+import common
+
+struct CatalogueProductCategoryView: View {
+    
+    var category: ProductCategory
+    
+    var body: some View {
+        Group{
+            Text(category.name)
+                .foregroundStyle(Color.myPrimaryColor)
+                .padding(.vertical, 4)
+                .padding(.horizontal, 8)
+                .fontWeight(.medium)
+                .font(.caption)
+        }
+        .background(Color.myPrimarySurfaceColor)
+        .cornerRadius(10.0)
+        .padding(.vertical,8)
+    }
+}
+
+#Preview {
+    CatalogueProductCategoryView(category: product_categories[0])
+}
