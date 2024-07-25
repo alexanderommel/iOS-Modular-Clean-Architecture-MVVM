526d1a1fb09f7473fd59f19b6c98b91a362665ef
Rommel <alexanderommelsw@outlook.es>
2024-07-25 00:59:52 -0500

    Update ShoppingCartView.swift

--- features/checkout/checkout-ui/checkout-ui/views/ShoppingCartView.swift
+++ features/checkout/checkout-ui/checkout-ui/views/ShoppingCartView.swift
@@ -12,27 +12,27 @@ struct ShoppingCartView: View {
     var body: some View {
         VStack{
             HStack{
-                Image(systemName: "star")
+                Image(uiImage: loadImageFromAssets(name: "store1"))
                     .resizable()
-                    .frame(maxWidth: 48, maxHeight: 48)
+                    .frame(maxWidth: 62, maxHeight: 62)
                     .aspectRatio(contentMode: .fit)
                     .clipShape(.circle)
                 VStack(alignment: .leading){
                     Text("Sal, Pimienta y Hambrienta")
-                        .font(.subheadline)
+                        .font(.headline)
                         .fontWeight(.semibold)
                     HStack{
                         Text("2 items")
-                            .font(.subheadline)
-                            .fontWeight(.semibold)
-                            .foregroundStyle(Color.myText12Color)
+                            .font(.headline)
+                            .fontWeight(.medium)
+                            .foregroundStyle(Color.myText34Color)
                         Image(systemName: "circle.fill")
                             .resizable()
                             .frame(maxWidth: 8, maxHeight: 8)
                         Text("$5.50")
-                            .font(.subheadline)
-                            .fontWeight(.semibold)
-                            .foregroundStyle(Color.myText12Color)
+                            .font(.headline)
+                            .fontWeight(.medium)
+                            .foregroundStyle(Color.myText34Color)
                     }
                 }
                 .padding(.leading, 8)
@@ -42,11 +42,51 @@ struct ShoppingCartView: View {
                     .frame(maxWidth: 32, maxHeight: 32)
                     .aspectRatio(contentMode: .fit)
                     .clipShape(.circle)
-                    .foregroundStyle(.red)
+                    .foregroundStyle(.black)
             }
-            .padding(10)
+            .padding(14)
+            
+            Button(
+                action: {
+                    
+
+                }, label: {
+                    
+                    Text("View cart")
+                        .font(.body)
+                        .fontWeight(.medium)
+                        .frame(maxWidth: .infinity, maxHeight: 45)
+                        .foregroundColor(Color.white)
+                        .background(Color.black)
+                        .cornerRadius(10)
+                }
+            ).padding(.horizontal, 14)
+                .padding(.top, 2)
+            
+            Button(
+                action: {
+                    
+
+                }, label: {
+                    
+                    Text("View store")
+                        .font(.body)
+                        .fontWeight(.medium)
+                        .frame(maxWidth: .infinity, maxHeight: 45)
+                        .foregroundColor(Color.black)
+                        .background(Color.myPrimarySurfaceGrayColor)
+                        .cornerRadius(10)
+                }
+            ).padding(.horizontal, 14)
+                .padding(.top, 0)
+                .padding(.bottom, 14)
             
         }
+        
+        .background(Color.white)
+        .cornerRadius(12.0)
+        .shadow(color: Color(hue: 1.0, saturation: 0.027, brightness: 0.829),radius: 1)
+        
     }
 }
 
