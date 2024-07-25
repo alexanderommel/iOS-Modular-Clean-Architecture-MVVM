6797e922a466037a6a6ce812d90f0c0a59a8adda
Rommel <alexanderommelsw@outlook.es>
2024-07-25 00:59:46 -0500

    Create TestData.swift

---
+++ features/checkout/checkout/test/TestData.swift
@@ -0,0 +1,17 @@
+//
+//  TestData.swift
+//  checkout
+//
+//  Created by Rommel Valdiviezo on 25/7/24.
+//
+
+import Foundation
+import stores
+
+public let s1 = stors[0]
+
+
+public let shopping_carts = [
+    ShoppingCart(id: ObjectIdentifier(String.self), items: [], store: stors[0]),
+    ShoppingCart(id: ObjectIdentifier(String.self), items: [], store: stors[1])
+]
