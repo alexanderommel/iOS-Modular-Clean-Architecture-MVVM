2ed67310790005d4d082187fd5dc529af2e0f6b6
Rommel <alexanderommelsw@outlook.es>
2024-07-25 08:55:11 -0500

    Update ShoppingCart.swift

--- features/checkout/checkout/models/ShoppingCart.swift
+++ features/checkout/checkout/models/ShoppingCart.swift
@@ -6,12 +6,19 @@
 //
 
 import Foundation
+import common
 import stores
 
 public struct ShoppingCart: Identifiable{
     public var id: ObjectIdentifier
     public var items: [LineItem]
     public var store: Store
+    public var total_amount: Money{
+        get{
+            
+            return Money(amount: Decimal(10), currency: .USD)
+        }
+    }
     public init(id: ObjectIdentifier, items: [LineItem], store: Store) {
         self.id = id
         self.items = items
