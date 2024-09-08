82a9e0b175baa85c45545cb12ff1bcbb68ace4c3
Rommel <alexanderommelsw@outlook.es>
2024-09-08 13:14:28 -0500

    added remaining dependencies to the app environment

--- dandelion/dandelion/ContentView.swift
+++ dandelion/dandelion/ContentView.swift
@@ -6,26 +6,51 @@
 //
 
 import SwiftUI
-import stores
 import stores_ui
 import networking
 import common
+import stores
 import user
+import persistence
+
+import routing
+
 import checkout
 
 struct ContentView: View {
+    
+    @EnvironmentObject var router: NavigationRouter
+    
+    
     var body: some View {
-        StoresScreen()
+        NavigationStack(path: $router.navPath){
+            StoresScreen(user: test_user)
+                .setUpNav()
+        }
+        
     }
 }
 
 #Preview {
-    @StateObject var storesViewModel = StoresViewModel(api: StoresApiImpl(store_remote_repo: StoreRemoteRepositoryFaker(), user_api_interactor: UserApiInteractorFaker()), checkout_api: CheckoutApiInteractorFaker1())
+    
+    @StateObject var router = NavigationRouter()
+    
+    let context = AppDatabase.preview.container.newBackgroundContext()
+    
+    
+    @StateObject var storesViewModel = StoresViewModel(api: StoresApiImpl(store_remote_repo: StoreRemoteRepositoryFaker(), user_api_interactor: UserApiInteractorFaker()),
+                                        checkout_api: CheckoutApiInteractorImpl(storeApi: StoresApiImpl(store_remote_repo: StoreRemoteRepositoryFaker(), user_api_interactor: UserApiInteractorFaker()), userApi: UserApiInteractorFaker(), checkoutLocalRepo: CheckoutLocalRepositoryImpl(context: AppDatabase.preview.container.newBackgroundContext()))
+    )
+    
+    
     @StateObject var catalogueViewModel = CatalogueViewModel(api: StoresApiImpl(store_remote_repo: StoreRemoteRepositoryFaker(), user_api_interactor: UserApiInteractorFaker()))
-    @StateObject var productViewModel = ProductViewModel(api: CheckoutApiInteractorFaker1())
+    
+    
+    @StateObject var productViewModel = ProductViewModel(api: CheckoutApiInteractorImpl(storeApi: StoresApiImpl(store_remote_repo: StoreRemoteRepositoryFaker(), user_api_interactor: UserApiInteractorFaker()), userApi: UserApiInteractorFaker(), checkoutLocalRepo: CheckoutLocalRepositoryImpl(context: context)))
     
     return ContentView()
         .environmentObject(storesViewModel)
         .environmentObject(catalogueViewModel)
         .environmentObject(productViewModel)
+        .environmentObject(router)
 }
