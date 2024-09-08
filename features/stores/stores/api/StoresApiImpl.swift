2330092436e454aa924ae3a82c6cf8d965b26250
Rommel <alexanderommelsw@outlook.es>
2024-09-08 13:12:36 -0500

    Update StoresApiImpl.swift

--- features/stores/stores/api/StoresApiImpl.swift
+++ features/stores/stores/api/StoresApiImpl.swift
@@ -11,12 +11,20 @@ import user
 
 public class StoresApiImpl: StoresApiInteractor{
     
+    
+    
+    
+    
     let fetch_uc:FetchStoresUseCase
     let fetch_catalogue_uc: FetchCatalogueUseCase
+    let fetch_store_uc:FetchStoreUseCase
+    let fetch_product_uc: FetchProductUseCase
     
     public init(store_remote_repo: StoreRemoteRepository, user_api_interactor: UserApiInteractor) {
         self.fetch_uc = FetchStoresUseCase(repository: store_remote_repo, userApi: user_api_interactor)
         self.fetch_catalogue_uc = FetchCatalogueUseCase(repository: store_remote_repo)
+        self.fetch_store_uc = FetchStoreUseCase(repository: store_remote_repo, userApi: user_api_interactor)
+        self.fetch_product_uc = FetchProductUseCase(repository: store_remote_repo, userApi: user_api_interactor)
     }
     
     public func getStores() async -> UseCaseResponse<[Store]> {
@@ -27,5 +35,12 @@ public class StoresApiImpl: StoresApiInteractor{
         return await fetch_catalogue_uc.invoke(storeId: store.id)
     }
     
+    public func getStoreById(id: Int) async -> common.UseCaseResponse<Store> {
+        return await fetch_store_uc.invoke(id: id)
+    }
+    
+    public func getProductById(id: Int) async -> common.UseCaseResponse<Product> {
+        return await fetch_product_uc.invoke(id: id)
+    }
     
 }
