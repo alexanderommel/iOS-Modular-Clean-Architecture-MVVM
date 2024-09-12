8ad012aeefb0f24c3cfdf0e01b48c6fe29fd7899
Rommel <alexanderommelsw@outlook.es>
2024-09-12 16:08:43 -0500

    modules

---
+++ features/orders/orders-ui/orders-ui/orders_ui.h
@@ -0,0 +1,18 @@
+//
+//  orders_ui.h
+//  orders-ui
+//
+//  Created by Rommel Valdiviezo on 12/9/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for orders_ui.
+FOUNDATION_EXPORT double orders_uiVersionNumber;
+
+//! Project version string for orders_ui.
+FOUNDATION_EXPORT const unsigned char orders_uiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <orders_ui/PublicHeader.h>
+
+
